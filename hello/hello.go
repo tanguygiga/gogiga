42526efcccd817fc3f4fb2f4ffb2783ab0e48f34
tanguygiga <gigarelt@gmail.com>
2017-09-07 22:57:34 +0200

    retouche

--- hello/hello.go
+++ hello/hello.go
@@ -10,7 +10,7 @@ import (
 func main() {
 	res, _ := http.Get("https://api.ipify.org")
 	ip, _ := ioutil.ReadAll(res.Body)
-	fmt.Printf("Je m'appelle Tanguy Gigarel et voici\nMon premier programme en go !\n")
+	fmt.Printf("Je m'appelle Tanguy Giga et voici\nMon premier programme en go !\n")
 	fmt.Printf("Ceci est mon adresse ip publique :\n")
 	os.Stdout.Write(ip)
 	fmt.Printf("\n")
